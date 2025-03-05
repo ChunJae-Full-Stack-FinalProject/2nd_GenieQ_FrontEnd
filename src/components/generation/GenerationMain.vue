a6c07c9ad467b59b9ec63a80a08578d23a5478bc
my2min0 <ffrf1234tina@gmail.com>
2025-03-05 20:06:14 +0900

    Comment: 주석 삭제

--- src/components/generation/GenerationMain.vue
+++ src/components/generation/GenerationMain.vue
@@ -1,18 +1,14 @@
 <template>
     <div class="app-container">
-        <!-- <Sidebar/> 컴포넌트 추가 -->
         <div class="main-content">
             <CreatePassageMain/>
             <PaymentUsage/>
-            <!-- <ResetComponent id="reset-component"/> //컴포넌트 추가 -->
-            <!-- <CreateComponent id="create-component"/> //컴포넌트 추가 -->
         </div>
     </div>
 </template>
 <script setup>
 import CreatePassageMain from '@/components/generation/passage/CreatePassageMain.vue';
 import PaymentUsage from '@/components/generation/passage/PaymentUsage.vue';
-import BaseButton from '../common/button/BaseButton.vue';
 
 </script>
 <style scoped>
