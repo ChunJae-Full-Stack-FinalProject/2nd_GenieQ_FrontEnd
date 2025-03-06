c6738695132249b2234e18e3fc8d3d67406c6834
my2min0 <ffrf1234tina@gmail.com>
2025-03-06 16:58:03 +0900

    Feat: 문항 생성 전 화면 구현 완료

---
+++ src/components/generation/question/QuestionMain/UserInsertPassage.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <textarea id="user-passage-text" placeholder="지문을 입력해주세요."></textarea>
+    </div>
+</template>
+<script setup>
+
+const userInput = ref('');
+</script>
+<style scoped>
+#user-passage-text {
+    width: 756px;
+    height: 358px;
+
+    font-family: 'Pretendard';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 28px;
+
+    letter-spacing: -0.02em;
+    color: #303030;
+
+    flex: none;
+    order: 0;
+    align-self: stretch;
+    flex-grow: 1;
+
+    border: none;
+    outline: none; /* 포커스 된 경우의 파란 테두리 삭제 */
+    resize: none;
+}
+</style>
