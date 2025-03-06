07fe0566b6a7d77bbeee279a1f0212bc90be5c6f
woogamjaa <woogamjaa@gmail.com>
2025-03-06 17:39:00 +0900

    Feat: 메인화면, 노티리스트compo, 인포compo 구현, tofaq 구현중

---
+++ src/components/mainhome/MainContent/ToFAQ.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="tofaq-container">
+        <div class="top-container">
+            <div class="notice-title">자주 묻는 질문 확인하기</div>
+            <Icon icon="weui:arrow-outlined" width="24" height="24" id="arrow-icon" style="color: #303030" />
+        </div>
+    </div>
+</template>
+<script setup>
+import { Icon } from "@iconify/vue";
+</script>
+<style>
+.tofaq-container {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    padding: 20px;
+    gap: 12px;
+    isolation: isolate;
+    position: absolute;
+    width: 460px;
+    height: 132px;
+    left: 1245px;
+    top: 48px;
+    background: #FFFFFF;
+    border-radius: 8px;
+    box-sizing: border-box;
+}
+</style>
---
+++ src/components/mainhome/MainHome.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="app-container">
+        <div class="main-content">
+            <WelcomInfo/>
+            <NoticeList/>
+            <ToFAQ/>
+        </div>
+    </div>
+</template>
+<script setup>
+import WelcomInfo from '@/components/mainhome/MainContent/WelcomInfo.vue'
+import NoticeList from '@/components/mainhome/MainContent/NoticeList.vue'
+import ToFAQ from '@/components/mainhome/MainContent/ToFAQ.vue'
+</script>
+<style>
+    
+</style>
+
+
