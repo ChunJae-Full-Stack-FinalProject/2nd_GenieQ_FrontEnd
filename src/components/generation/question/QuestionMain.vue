70a8bfd88169352a61a48eafba718b90fefa9b03
my2min0 <ffrf1234tina@gmail.com>
2025-03-06 15:13:24 +0900

    Feat: QuestionMain 화면 생성 및 라우터 연결

---
+++ src/components/generation/question/QuestionMain.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="app-container">
+        <div class="main-content">
+            
+        </div>
+    </div>
+</template>
+<script setup>
+
+</script>
+<style scoped>
+    
+</style>
