a1fae398b6d35c9d80f1b096ad174cdac1df62ba
woogamjaa <woogamjaa@gmail.com>
2025-03-07 13:02:10 +0900

    Merge branch 'dev' of https://github.com/ChunJae-Full-Stack-FinalProject/2nd_GenieQ_FrontEnd into 33-SNB

--- src/components/mainhome/MainContent/ToFAQ.vue
+++ src/components/mainhome/MainContent/ToFAQ.vue
@@ -4,12 +4,17 @@
             
             <div class="tofaq-title">자주 묻는 질문 확인하기</div>
             
+            <router-link :to="{ name: 'my-page-wrapper', params: { 'tab': '자주 묻는 질문' } }">
             <Icon icon="weui:arrow-outlined" width="24" height="24" id="arrow-icon" style="color: #ffffff" />
+            </Router-Link>
         </div>
     </div>
 </template>
 <script setup>
 import { Icon } from "@iconify/vue";
+import { ref } from 'vue';
+
+const faqTab = ref('자주 묻는 질문');
 </script>
 <style>
 .tofaq-container {
