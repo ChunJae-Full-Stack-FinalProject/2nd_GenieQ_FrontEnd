78c9a87e24e858bb50ffa84c03c10d671e7d1920
my2min0 <ffrf1234tina@gmail.com>
2025-03-08 00:27:43 +0900

    Design: myinfo 수정 완료~

--- src/components/mypage/Info.vue
+++ src/components/mypage/Info.vue
@@ -1,8 +1,6 @@
 <template>
   <div class="wrapper">
-    <div class="button">
-      <div class="button-type1">저장하기</div>
-    </div>
+    <BaseButton text="저장하기" type="type1" id="save-button" width="248px" height="46px"/>
 
     <div class="content">
       <!-- 기본 정보 -->
@@ -21,6 +19,7 @@
         <div class="item">
           <span class="label">비밀번호</span>
           <span class="value">****</span>
+          <BaseButton text="비밀번호 변경" type="type3" id="pwd-change"width="125px" height="40px" />
         </div>
       </section>
 
@@ -35,7 +34,16 @@
         </div>
         <div class="item">
           <span class="label">소속</span>
-          <span class="value value-box">고등교사</span>
+          <select class="value value-box" id="user-type">
+            <option value="elementary">초등교사</option>
+            <option value="middle">중등교사</option>
+            <option value="high">고등교사</option>
+            <option value="academy">학원</option>
+            <option value="company">기업</option>
+            <option value="student">학생</option>
+            <option value="parent">학부모</option>
+            <option value="other">기타</option>
+          </select>
         </div>
       </section>
     </div>
@@ -46,36 +54,36 @@
     </div>
   </div>
 </template>
-
+<script setup>
+import BaseButton from '../common/button/BaseButton.vue';
+</script>
 <style scoped>
 /* 전체 레이아웃 */
 .wrapper {
-  width: 100%; 
-  max-width: 1764px; 
-  height: 700px;
-  margin: 0 auto;
-}
+  position: absolute;
+  top: 0px;
+  left: 0px;
 
-/* 버튼 */
-.button {
   width: 1764px;
-  height: 46px;
 }
-.button-type1 {
-  width: 248px;
-  height: 46px;
+#save-button {
+  position: absolute;
+  top:24px;
+  left: 1360px;
 }
 
 .content {
-  width: 1472px;
-  height: auto; 
   display: flex;
   flex-direction: column;
-  gap: 20px;
-  padding-bottom: 80px;
-  padding-top: 50px;
-  position: relative; 
-  
+  align-items: flex-start;
+  padding: 0px;
+  gap: 40px;
+
+  position: absolute;
+  width: 1472px;
+  height: 384px;
+  left: 136px;
+  top: 94px;
 }
 
 .content::after {
@@ -93,31 +101,51 @@
 /* 기본 정보, 추가 정보 공통 박스 스타일 */
 .basic, .additional {
   width: 1472px;
+
   background: #FFFFFF;
   border-radius: 12px;
-  padding: 20px;  /* 내부 여백 */
+  padding: 8px 20px;  /* 내부 여백 */
+
   display: flex;
   flex-direction: column;
-  gap: 10px;
-  margin-left: 150px;
+  align-items: flex-start;
+  gap: 4px;
 }
 
 /* 기본 정보 박스 */
 .basic {
   height: 194px;
+
+  flex: none;
+  order: 0;
+  align-self: stretch;
+  flex-grow: 0;
 }
 
 /* 추가 정보 박스 */
 .additional {
   height: 150px;
-  margin-top: 25px;
+
+  flex: none;
+  order: 1;
+  align-self: stretch;
+  flex-grow: 0;
 }
 
 /* 타이틀 */
 .title-container {
   display: flex;
+  flex-direction: row;
   align-items: center;
-  width: 100%;
+  padding: 8px;
+  gap: 8px;
+  border-bottom: 1px solid #E1E1E1;
+
+  width: 1432px;
+
+  flex: none;
+  order: 0;
+  flex-grow: 0;
 }
 .title {
   font-family: 'Pretendard';
@@ -130,22 +158,54 @@
 
 /* 기본 정보, 추가 정보 내용 스타일 */
 .item {
-  width: 100%;
   display: flex;
-  justify-content: flex-start; /* 왼쪽 정렬 */
-  gap: 20px; 
+  flex-direction: row;
+  align-items: center;
+  padding: 0px;
+  gap: 160px;
 
-  font-family: 'Pretendard';
-  font-weight: 500;
-  font-size: 16px;
-  line-height: 150%;
-  letter-spacing: -2%;
+  width: 1432px;
+  height: 40px;
+
+  flex: none;
+  order: 1;
+  align-self: stretch;
+  flex-grow: 0;
 }
 
 /* 라벨 및 값 스타일 */
 .label {
-  width: 120px;
-  color: #424242;
+display: flex;
+flex-direction: row;
+align-items: center;
+padding: 8px;
+gap: 8px;
+
+width: 120px;
+height: 40px;
+
+font-family: 'Pretendard';
+font-style: normal;
+font-weight: 500;
+font-size: 16px;
+line-height: 150%;
+
+letter-spacing: -0.02em;
+color: #424242;
+}
+.value {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: 8px;
+  gap: 8px;
+
+  width: 360px;
+  height: 40px;
+
+  flex: none;
+  order: 1;
+  flex-grow: 0;
 }
 .value-box {
   display: flex;
@@ -157,7 +217,7 @@
   height: 40px;
 
   background: #FFFFFF; /* 배경색 */
-  border: 1px solid #BDBDBD; /* 테두리 */
+  border: 1px solid #FF9F40; /* 테두리 */
   border-radius: 8px; /* 모서리 둥글게 */
 
   flex: none;
@@ -180,6 +240,22 @@
   margin-left: auto;
   margin-right: 110px; 
   margin-top: 90px;
+}
+#pwd-change {
+  left: 1000px;
+  font-family: 'Pretendard';
+  font-style: normal;
+  font-weight: 600;
+  font-size: 16px;
+  line-height: 150%;
+
+  text-align: center;
+  letter-spacing: -0.02em;
+
+  color: #424242;
+
+  position: absolute;
+  left: 535px;
 }
 
 .withdrawal {
