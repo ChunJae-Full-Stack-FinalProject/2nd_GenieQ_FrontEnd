72429565075a7bac612760fec2d931a0f01b15df
woogamjaa <woogamjaa@gmail.com>
2025-03-09 02:16:43 +0900

    Design: 공지사항 게시글 연동 허술하지만 성공.

--- src/router/mypage.routes.js
+++ src/router/mypage.routes.js
@@ -1,4 +1,7 @@
 import myPageWrapper from "@/components/mypage/MyPageWrapper.vue";
+import Notice from "@/components/mypage/Notice.vue";
+import NoticeDetail from "@/components/mypage/NoticeDetail.vue";
+
 
 const myPageRoutes = [
     {
@@ -6,8 +9,21 @@ const myPageRoutes = [
       name: 'mypage',
       children: [
         { path: '', name: 'my-page-wrapper', component: myPageWrapper },
+        
       ]
-    }
+    },
+    {
+      path: '/notice',
+      name: 'Notice',
+      component: Notice
+    },
+    { path: '/notice/:id', name: 'NoticeDetail', component: NoticeDetail},
   ];
   
   export default myPageRoutes;
+
+
+ 
+ 
+
+  
