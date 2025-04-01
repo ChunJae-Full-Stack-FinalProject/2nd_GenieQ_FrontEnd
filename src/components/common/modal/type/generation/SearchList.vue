e862b3390c204df45436a9609a481d730011cd6d
hoonee-math <kwanghoon041@gmail.com>
2025-04-02 01:16:37 +0900

    Fix: 지문 불러오기 오류 해결

--- src/components/common/modal/type/generation/SearchList.vue
+++ src/components/common/modal/type/generation/SearchList.vue
@@ -39,8 +39,10 @@ const emit = defineEmits(["preview"]);
 const activeItem = ref(null);
 
 watch(() => props.activeItemId, (newVal) => {
+  // console.log('[SearchList] activeItemId 변경됨:', newVal);
   if (newVal) {
     activeItem.value = newVal;
+    // console.log('[SearchList] activeItem 설정됨:', activeItem.value);
 
     // 활성화된 항목으로 화면 포커싱
     setTimeout(() => {
@@ -54,8 +56,15 @@ watch(() => props.activeItemId, (newVal) => {
 
 // ✅ 탭이 변경될 때 첫 번째 항목 활성화 처리
 watch(() => props.items, (newItems) => {
+  // console.log('[SearchList] items 변경됨, 길이:', newItems.length);
   if (newItems.length > 0) {
-    activeItem.value = newItems[0].PAS_CODE;
+    // activeItem.value = newItems[0].PAS_CODE; // 주석 처리 - 부모의 activeItemId 우선시
+    
+    // 부모로부터 받은 activeItemId가 없을 때만 첫 번째 항목 활성화
+    if (!props.activeItemId) {
+      activeItem.value = newItems[0].PAS_CODE;
+      // console.log('[SearchList] 첫 번째 항목 활성화:', activeItem.value);
+    }
   }
 }, { immediate: true });
 
@@ -70,8 +79,13 @@ onMounted(() => {
 });
 
 const toggleActive = (PAS_CODE) => {
+  // console.log('[SearchList] 항목 클릭됨:', PAS_CODE);
   if (activeItem.value !== PAS_CODE) {
     activeItem.value = PAS_CODE;
+    // console.log('[SearchList] 활성 항목 변경됨:', activeItem.value);
+    
+    // 부모에게 활성화 ID 변경 알림 추가
+    emit("activeItemChange", PAS_CODE);
   }
 };
 
